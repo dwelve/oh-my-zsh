95a515e24e64d00bc5f6c3ddeae65cd306e08575
Lue Andrie-Her <lue.her@gmail.com>
2012-07-16 06:11:48 -0700

    Add tmux to autostart.

--- modules/tmux/init.zsh
+++ modules/tmux/init.zsh
@@ -15,7 +15,7 @@ if [[ -z "$TMUX" ]] && zstyle -t ':omz:module:tmux' auto-start; then
     tmux set-option -g destroy-unattached off &> /dev/null
 
     # Create a new session.
-    tmux new-session -d -s "$tmux_session"
+    tmux -2 new-session -d -s "$tmux_session"
 
     # Disable the destruction of the new, unattached session.
     tmux set-option -t "$tmux_session" destroy-unattached off &> /dev/null
@@ -25,7 +25,7 @@ if [[ -z "$TMUX" ]] && zstyle -t ':omz:module:tmux' auto-start; then
     tmux set-option -g destroy-unattached on &> /dev/null
   fi
 
-  exec tmux new-session -t "$tmux_session"
+  exec tmux -2 new-session -t "$tmux_session"
 fi
 
 # Aliases
